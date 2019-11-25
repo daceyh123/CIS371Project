135b7e82dc17c651595b4368a16dd3988e92df6a
daceyh123 <36930977+daceyh123@users.noreply.github.com>
2019-11-24 22:25:08 -0500

    Asteroids Works (needs to be centered and adjusted)

--- template-vue/src/route-init.js
+++ template-vue/src/route-init.js
@@ -1,6 +1,7 @@
 //import Budget from "@/components/Budget.vue";
 import Login from "@/components/Login.vue";
 import Snake from "@/components/Snake.vue";
+import Asteroid from "@/components/asteroid.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
 
@@ -8,7 +9,8 @@ Vue.use(VueRouter);
 /* Map path to component */
 const routeTable = [
     { path: "/", component: Login },
-    { path: "/snake", component: Snake}
+    { path: "/snake", component: Snake},
+    { path: "/asteroid", component: Asteroid}
     //{ path: "/budget", component: Budget }
 ];
 
