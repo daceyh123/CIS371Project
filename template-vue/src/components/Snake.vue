6e88492d1b44a958d03ff633582760e64dc488a4
daceyh123 <36930977+daceyh123@users.noreply.github.com>
2019-11-24 22:09:36 -0500

    Snake works, does not fit in box though
    Snake works

--- template-vue/src/components/Snake.vue
+++ template-vue/src/components/Snake.vue
@@ -1,8 +1,159 @@
 <template>
-<body>
-    <canvas id="snake" width="608" height="608"></canvas>
-</body>
+    <canvas ref="snake" id="snake" width="608" height="608"></canvas>
 </template>
 <script>
+const ground = new Image();
+    ground.src = require('../assets/img/ground.png');
 
+    const foodImg = new Image();
+    foodImg.src = require('../assets/img/food.png');
+    //import {Image as food1} from "@/components/img/food.png"; 
+    //import {Image as ground1} from "@/components/img/ground.png";
+export default {
+    data: () => ({
+    }),
+    mounted() {
+    const cvs = this.$refs.snake;
+    //alert(cvs)
+    const ctx = cvs.getContext("2d");
+
+    // create unit
+    const box = 32;
+
+    // load images
+    
+    //print(foodImg)
+    // load audio files
+    let dead = new Audio();
+    let eat = new Audio();
+    let up = new Audio();
+    let right = new Audio();
+    let left = new Audio();
+    let down = new Audio();
+
+    dead.src = "audio/dead.mp3";
+    eat.src = "audio/eat.mp3";
+    up.src = "audio/up.mp3";
+    right.src = "audio/right.mp3";
+    left.src = "audio/left.mp3";
+    down.src = "audio/down.mp3";
+
+    // create the snake
+    let snake  = [];
+
+    snake[0] = {
+        x : 9 * box,
+        y : 10 * box
+    };
+
+    // create food
+    let food = {
+        x : Math.floor(Math.random() * 17 + 1) * box,
+        y : Math.floor(Math.random() * 15 + 3) * box
+    }
+
+    // create score 
+    let score = 0;
+
+    //control the snake
+    let d;
+
+    document.addEventListener("keydown",direction);
+
+    function direction(event) {
+        let key = event.keyCode;
+
+        if (key == 37 && d != "RIGHT") {
+            left.play();
+            d = "LEFT";
+        }
+        else if (key == 38 && d != "DOWN") {
+            d = "UP";
+            up.play()
+        }
+        else if (key == 39 && d != "LEFT") {
+            d = "RIGHT";
+            right.play();
+        }
+        else if (key == 40 && d != "UP") {
+            d = "DOWN";
+            down.play();
+        }
+    }
+
+    // check collision function
+    function collision(head,array) {
+        for (let i = 0; i < array.length; i++) {
+            if (head.x == array[i].x && head.y == array[i].y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // draw everyhting to canvas
+    function draw() {
+        ctx.drawImage(ground, 0 , 0);
+
+        for (let i = 0; i < snake.length; i++) {
+            ctx.fillStyle = (i == 0)? "green" : "white";
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+        }
+
+        ctx.drawImage(foodImg, food.x, food.y);
+
+        // old head position
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        // which direction
+        if ( d == "LEFT") snakeX -= box;
+        if ( d == "UP") snakeY -= box;
+        if ( d == "RIGHT") snakeX += box;
+        if ( d == "DOWN") snakeY += box;
+
+        // if the snake eats the food
+        if (snakeX == food.x && snakeY == food.y) {
+            score++;
+
+            eat.play();
+
+            food = {
+                x : Math.floor(Math.random() * 17 + 1) * box,
+                y : Math.floor(Math.random() * 15 + 3) * box
+            }
+        }
+        else {
+        // remove tail
+        snake.pop();
+        }
+
+        // add new head
+        let newHead = {
+            x : snakeX,
+            y : snakeY
+        }
+
+        // game over
+        if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 17 * box || collision(newHead,snake)) {
+            clearInterval(game);
+            dead.play();
+        }
+
+        snake.unshift(newHead);
+
+        ctx.fillStyle = "white";
+        ctx.font = "45px Changa one";
+        ctx.fillText(score, 2 * box, 1.6 * box);
+    }
+
+    // call draw funtion every 100 ms
+    let game = setInterval(draw,100);
+    },
+    methods: {
+    }
+  }
 </script>
