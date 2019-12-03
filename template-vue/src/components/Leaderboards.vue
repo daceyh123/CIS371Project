b9be800c98370e0b7cb87a610c44d2842de08ffa
daceyh123 <36930977+daceyh123@users.noreply.github.com>
2019-12-03 00:12:21 -0500

    leaderboards now all one board

--- template-vue/src/components/Leaderboards.vue
+++ template-vue/src/components/Leaderboards.vue
@@ -1,62 +1,64 @@
 <template>
   <div class="wrapper">
-    <div class="one">
-      <h3>Snake</h3>
-      <v-simple-table id="scoretable">
-        <thead>
-          <tr id="snake">
-            <th>Ranking</th>
-            <th>Score</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(snakeScores,pos) in snakeScores" :key="pos">
-            <td v-bind:ref="pos">{{pos +1 }}</td>
-            <td>{{snakeScores.Score}}</td>
-            <td>{{snakeScores.User}}</td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </div>
-    <div class="two">
-      <h3>Asteroids</h3>
-      <v-simple-table id="scoretable">
-        <thead>
-          <tr id="asteroids">
-            <th>Ranking</th>
-            <th>Score</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(asteroidScores,pos) in asteroidScores" :key="pos">
-            <td v-bind:ref="pos">{{pos +1 }}</td>
-            <td>{{asteroidScores.Score}}</td>
-            <td>{{asteroidScores.User}}</td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </div>
-    <div class="three">
-      <h3>Flappy Bat</h3>
-      <v-simple-table id="scoretable">
-        <thead>
-          <tr id="flap">
-            <th>Ranking</th>
-            <th>Score</th>
-            <th>Username</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(flapBatScores,pos) in flapBatScores" :key="pos">
-            <td v-bind:ref="pos">{{pos +1 }}</td>
-            <td>{{flapBatScores.Score}}</td>
-            <td>{{flapBatScores.User}}</td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-    </div>
+    <v-tabs color="primary">
+      <v-tab>Asteroids</v-tab>
+      <v-tab>Snake</v-tab>
+      <v-tab>Flappy Bat</v-tab>
+      <v-tab-item>
+        <v-simple-table id="scoretable">
+          <thead>
+            <tr id="asteroids">
+              <th>Ranking</th>
+              <th>Score</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(asteroidScores,pos) in asteroidScores" :key="pos">
+              <td v-bind:ref="pos">{{pos +1 }}</td>
+              <td>{{asteroidScores.Score}}</td>
+              <td>{{asteroidScores.User}}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-tab-item>
+      <v-tab-item>
+        <v-simple-table id="scoretable">
+          <thead>
+            <tr id="snake">
+              <th>Ranking</th>
+              <th>Score</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(snakeScores,pos) in snakeScores" :key="pos">
+              <td v-bind:ref="pos">{{pos +1 }}</td>
+              <td>{{snakeScores.Score}}</td>
+              <td>{{snakeScores.User}}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-tab-item>
+      <v-tab-item>
+        <v-simple-table id="scoretable">
+          <thead>
+            <tr id="flap">
+              <th>Ranking</th>
+              <th>Score</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(flapBatScores,pos) in flapBatScores" :key="pos">
+              <td v-bind:ref="pos">{{pos +1 }}</td>
+              <td>{{flapBatScores.Score}}</td>
+              <td>{{flapBatScores.User}}</td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-tab-item>
+    </v-tabs>
   </div>
 </template>
 <script>
@@ -81,17 +83,17 @@ export default {
       if (item.Game == "Snake") {
         this.snakeScores.push({ ...item });
         this.snakeScores.sort((a, b) => (a.Score < b.Score ? 1 : -1));
-        this.snakeScores = this.snakeScores.slice(0, 5);
+        this.snakeScores = this.snakeScores.slice(0, 8);
       }
       if (item.Game == "Asteroids") {
         this.asteroidScores.push({ ...item });
         this.asteroidScores.sort((a, b) => (a.Score < b.Score ? 1 : -1));
-        this.asteroidScores = this.asteroidScores.slice(0, 5);
+        this.asteroidScores = this.asteroidScores.slice(0, 8);
       }
       if (item.Game == "FlappyBat") {
         this.flapBatScores.push({ ...item });
         this.flapBatScores.sort((a, b) => (a.Score < b.Score ? 1 : -1));
-        this.flapBatScores = this.flapBatScores.slice(0, 5);
+        this.flapBatScores = this.flapBatScores.slice(0, 8);
       }
     }
     // getRank() {
@@ -113,7 +115,7 @@ export default {
 <style>
 .wrapper {
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 10px;
   grid-auto-rows: auto;
 }
